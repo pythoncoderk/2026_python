a1fd2efee1231b1a785e01e0c6ee7d78e25b2780
pythoncoderk <turntabroz@gmail.com>
2026-01-22 14:17:45 +0900

    atcoder

---
+++ 0122_2.py
@@ -0,0 +1,6 @@
+sec = 365 * 24 * 60 * 60
+
+print(sec * 1)
+print(sec * 2)
+print(sec * 5)
+print(sec * 10)
---
+++ 0122_6.py
@@ -0,0 +1,6 @@
+if True:
+    print("OK")
+if False:
+    print("NG")
+else:
+    print("OK")
