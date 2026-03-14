c0dccd11820aff41e1e062dd285c86922246830a
pythoncoderk <turntabroz@gmail.com>
2026-03-14 12:02:34 +0900

    atcoder

---
+++ 0314_1.py
@@ -0,0 +1,20 @@
+a = int(input())
+print(a // 150)
+
+
+
+b, c = map(int, input().split())
+print(b + c)
+
+
+
+d = int(input())
+e = int(input())
+
+print(d, e)
+
+
+d = int(input())
+e = int(input())
+
+print(d, e)
